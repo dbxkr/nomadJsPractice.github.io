021061aa6876e2711e18d0b69bf4a98b67b3c925
dbxkr <70328002+dbxkr@users.noreply.github.com>
2022-02-21 02:42:34 +0900

    Update background.js

--- background.js
+++ background.js
@@ -4,6 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `../image/${chosenImage}`;
+bgImage.src = `image/${chosenImage}`;
 
 document.body.appendChild(bgImage);
